8881441ee5b86a511efa0e618b633e64c42716cb
Miguel Arrieta <arrietafm@gmail.com>
2025-02-24 08:50:18 -0600

    Tarea1

--- 17_02_25/app.js
+++ 17_02_25/app.js
@@ -1,22 +1,35 @@
 //Operadores logicos
-console.log("Operador AND");
-
+console.log("--------------------------")
+console.log("|      Operador AND      |");
+console.log("--------------------------")
 let and0=false&&false;
 let and1=false&&true;
 let and2=true&&false;
 let and3=true&&true;
-console.log("false "+"&&"+" false="+and0);
-console.log("false "+"&&"+" true="+and1);
-console.log("true "+"&&"+" false="+and2);
-console.log("true "+"&&"+" true="+and3);
+console.log("| false "+"&&"+" false |=|"+and0+"|");
+console.log("| false "+"&&"+" true  |=|"+and1+"|");
+console.log("| true "+"&&"+" false  |=|"+and2+"|");
+console.log("| true "+"&&"+" true   |=| "+and3+"|");
+console.log("--------------------------")
 
-console.log("Operador OR");
 
-let or0=false&&false;
-let or1=false&&true;
-let or2=true&&false;
-let or3=true&&true;
-console.log("false "+"||"+" false="+or0);
-console.log("false "+"||"+" true="+or1);
-console.log("true "+"||"+" false="+or2);
-console.log("true "+"||"+" true="+or3);
+console.log("|      Operador OR       |");
+console.log("--------------------------")
+let or0=false||false;
+let or1=false||true;
+let or2=true||false;
+let or3=true||true;
+console.log("| false "+"||"+" false |=|"+or0+"|");
+console.log("| false "+"||"+" true  |=| "+or1+"|");
+console.log("| true "+"||"+" false  |=| "+or2+"|");
+console.log("| true "+"||"+" true   |=| "+or3+"|");
+
+console.log("--------------------------")
+console.log("|     Operador NOT       |");
+console.log("--------------------------")
+let not0=!false;
+let not1=!true;
+console.log("|     !false    |=| "+not0+" |");
+console.log("|     !true     |=| "+not1+"|");
+console.log("---------------------------")
+
