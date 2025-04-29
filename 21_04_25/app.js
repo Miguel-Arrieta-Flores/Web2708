3b00897a82ef78b6d795647eaf4765d06a92a4ee
Miguel Arrieta <arrietafm@gmail.com>
2025-04-28 19:22:48 -0600

    Tarea_4

--- 21_04_25/app.js
+++ 21_04_25/app.js
@@ -2,7 +2,7 @@ function generar(value){
     let numTel = document.getElementById('numTel').value.trim();
     let sms = encodeURIComponent(document.getElementById('mensaje').value.trim());
     var linkApi = `https://wa.me/${numTel}?text=${sms}`;
-    if(numTel ===   ''&& sms === ''){
+    if(numTel === '' || sms === ''){
         alert("Rellena los campos, por favor");
     }else{
         open(linkApi);
